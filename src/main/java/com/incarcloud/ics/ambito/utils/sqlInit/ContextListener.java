fa971eb35da9ac5699078f942852f4b4e26bc07d
Li2017 <xlli@incarcloud.com>
2019-01-07 10:27:17 +0800

    启动的时候加载sql

---
+++ src/main/java/com/incarcloud/ics/ambito/utils/sqlInit/ContextListener.java
@@ -0,0 +1,28 @@
+package com.incarcloud.ics.ambito.utils.sqlInit;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+
+@Component
+public class ContextListener implements ServletContextListener {
+    @Autowired
+    private InitSqlScript initSqlScript;
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("---------->初始化sql");
+        try {
+            initSqlScript.executeSqlFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("---------->初始化完成");
+    }
+
+
+}
--- src/main/resources/db/migration/V99999999__init.sql
+++ src/main/resources/db/migration/V99999999__init.sql
@@ -1,9 +1,9 @@
-insert into t_sys_user (id,username,password,real_name,gender,email,phone,state,salt)
+insert IGNORE into t_sys_user (id,username,password,real_name,gender,email,phone,state,salt)
 values
 (1,'admin','LoiAWka1PI+uJwjCQvkvJg==','管理员',1,'','',0,'1234');
 
 
-INSERT INTO t_sys_role (id, role_name)
+INSERT IGNORE INTO t_sys_role (id, role_name)
 VALUES
 (1, '超级管理员');
 
