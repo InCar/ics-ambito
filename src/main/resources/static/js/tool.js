1e40f1cb4ffc89e6b7a76c0e9d90564aca4d43a1
wellingtonjone <509750512@qq.com>
2019-01-31 11:24:18 +0800

    treeGrid

--- src/main/resources/static/js/tool.js
+++ src/main/resources/static/js/tool.js
@@ -29,48 +29,100 @@ const extend = (o,n,override) => {
 	}
 	return o;
 }
+const joinPara = (url, opt) => {
+  let date = new Date().getTime();
+  url += '?';
+  for(let key in opt) {
+    url += `${key}=${opt[key]}&`
+  }  
+   url += `timestamp=${date}`;
+   return url;
+}
+// const Ajax = {
+//   get: function(url, opt, success, fail) {
+//     let endUrl = joinPara(url, opt);
+//     // let date = new Date().getTime();
+//     var xhr = new XMLHttpRequest();
+//   //   url += '?' 
+//   //  for(let key in opt) {
+//   //    url += `${key}=${opt[key]}&`
+//   //  }  
+//   //   url += `timestamp=${date}`;
+//     xhr.open('GET', endUrl, true);
+//     xhr.onreadystatechange = function() {
+// 			// readyState == 4说明请求已完成
+// 			if (xhr.readyState == 4) {
+// 				if (xhr.status == 200 || xhr.status == 304) { 
+// 					// 从服务器获得数据 
+// 					success.call(this, xhr.responseText);  
+// 				} else {
+// 					fail.call(this, xhr.responseText);  
+// 				}
+// 			}
+//     };
+//     xhr.send();
+//   },
+//   // datat应为'a=a1&b=b1'这种字符串格式，在jq里如果data为对象会自动将对象转成这种字符串格式
+//   post: function (url, data, success, fail) {
+//     var xhr = new XMLHttpRequest();
+//     xhr.open("POST", url, true);
+//     // 添加http头，发送信息至服务器时内容编码类型
+//     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");  
+//     xhr.onreadystatechange = function() {
+//       if (xhr.readyState == 4) {
+// 				if (xhr.status == 200 || xhr.status == 304) { 
+// 					// 从服务器获得数据 
+// 					success.call(this, xhr.responseText);  
+// 				} else {
+// 					fail.call(this, xhr.responseText);  
+// 				}
+// 			}
+//     };
+//     xhr.send(data);
+//   }
+// }
 
-const Ajax = {
-  get: function(url, opt, success, fail) {
-    let date = new Date().getTime();
-    var xhr = new XMLHttpRequest();
-    url += '?' 
-   for(let key in opt) {
-     url += `${key}=${opt[key]}&`
-   }  
-    url += `timestamp=${date}`;
-    xhr.open('GET', url, true);
-    xhr.onreadystatechange = function() {
-			// readyState == 4说明请求已完成
-			if (xhr.readyState == 4) {
-				if (xhr.status == 200 || xhr.status == 304) { 
+const Ajax = (url,opt,methods) => {
+  return new Promise(function(resove,reject){
+    methods = methods || 'POST';
+    var xmlHttp = null;
+    if (XMLHttpRequest) {
+      xmlHttp = new XMLHttpRequest();
+    } else {
+      xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
+    };
+    var params = [];
+    for (var key in opt){
+      if(!!opt[key] || opt[key] === 0){
+        params.push(key + '=' + opt[key]);
+      }
+    };
+    var postData = params.join('&');
+    if (methods.toUpperCase() === 'POST') {
+      xmlHttp.open('POST', url, true);
+      xmlHttp.setRequestHeader('Content-Type', 'application/json;charset=utf-8');
+      xmlHttp.send(JSON.stringify(opt));
+    }else if (methods.toUpperCase() === 'GET') {
+      xmlHttp.open('GET', url + '?' + postData, true);
+      xmlHttp.send(null);
+    }else if(methods.toUpperCase() === 'DELETE'){
+      xmlHttp.open('DELETE', url + '?' + postData, true);
+      xmlHttp.send(null);
+    }
+    xmlHttp.onreadystatechange = function () {
+      // if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+      //   resove(JSON.parse(xmlHttp.responseText));
+      // }
+      if (xmlHttp.readyState == 4) {
+				if (xmlHttp.status == 200 || xmlHttp.status == 304) { 
 					// 从服务器获得数据 
-					success.call(this, xhr.responseText);  
+					resove(JSON.parse(xmlHttp.responseText));
 				} else {
-					fail.call(this, xhr.responseText);  
+					reject(JSON.parse(xmlHttp.responseText));
 				}
 			}
     };
-    xhr.send();
-  },
-  // datat应为'a=a1&b=b1'这种字符串格式，在jq里如果data为对象会自动将对象转成这种字符串格式
-  post: function (url, data, success, fail) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    // 添加http头，发送信息至服务器时内容编码类型
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");  
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState == 4) {
-				if (xhr.status == 200 || xhr.status == 304) { 
-					// 从服务器获得数据 
-					success.call(this, xhr.responseText);  
-				} else {
-					fail.call(this, xhr.responseText);  
-				}
-			}
-    };
-    xhr.send(data);
-  }
+  });
 }
 
  // deepCopy
@@ -98,7 +150,8 @@ const Ajax = {
 };
 export {
   DateFormat,
-	extend,
+  extend,
+  joinPara,
 	deepCopy,
   Ajax
 }
--- src/main/resources/static/js/tree.js
+++ src/main/resources/static/js/tree.js
@@ -4,24 +4,36 @@ export default {
 	constructor: this,
 	_initial: function(options) {
 		var par = {
-      treeName: '',
-      treeApi: ""
-      ,nodes: [{ //节点
-        name: '父节点1'
-        ,children: [{
-          name: '子节点11'
-        },{
-          name: '子节点12'
-        }]
-      },{
-        name: '父节点2（可以点左侧箭头，也可以双击标题）'
-        ,children: [{
-          name: '子节点21'
-          ,children: [{
-            name: '子节点211'
-          }]
-        }]
-      }]
+			elem: '',
+      apiUrl: "",
+			params: {id: 1}, // 传入id
+			addParams: { // 新增参数
+			},
+      config: {
+				 id: "",
+				 idField:'orgCode' //必須字段 
+				,method:'get'
+				,cellMinWidth: 100
+				,treeId:'orgCode'//树形id字段名称
+				,treeUpId:'parentCode'//树形父id字段名称
+				,treeShowName:'orgName'//以树形式显示的字段
+				,cols: [
+					{width:100,title: '操作', align:'center'/*toolbar: '#barDemo'*/
+							,templet: function(d){
+							var addBtn='<a class="layui-btn layui-btn-primary layui-btn-xs" lay-event="add">添加</a>';
+							var delBtn='<a class="layui-btn layui-btn-danger layui-btn-xs" lay-event="del">删除</a>';
+							return addBtn+delBtn;
+					}
+					}
+					,{field:'orgName', edit:'text',width:300, title: '组织名称'}
+			]
+				,page:false
+				,parseData:function (res) {//数据加载后回调
+				}
+				,onClickRow:function (index, o) {
+					// console.log(o)
+			}
+		}
     };
 		this.par = tool.extend(par, options, true);
 
@@ -29,13 +41,99 @@ export default {
     this.handlers = {};
     this.init(this.par);
 	},
-	init: function(obj){
-    tool.Ajax.get(`${obj.treeApi}/ics/sysorg/orgTree`, {}, (data) => {
-      console.log(data);
-    })
-		layui.use('tree', () => {
-      layui.tree(obj);
-    });
+	init: function(obj) {
+		layui.config({
+			base: '/dist/extend/'
+		}).extend({
+			treeGrid:'treeGrid'
+		}).use('treeGrid', () => {
+		this.treeGrid = layui.treeGrid;
+    this.treeGrid.render({
+			id: obj.config.id
+			,elem: obj.elem
+			,idField:obj.config.idField
+			,url:`${obj.apiUrl}/ics/sysorg/list`
+			,method:obj.config.method
+			,cellMinWidth: obj.config.cellMinWidth
+			,treeId:obj.config.treeId//树形id字段名称
+			,treeUpId:obj.config.treeUpId//树形父id字段名称
+			,treeShowName:obj.config.treeShowName//以树形式显示的字段
+			,cols: [obj.config.cols]
+			,page:obj.config.page
+			,parseData:function (res) {//数据加载后回调
+			}
+			,onClickRow:function (index, o) {
+				// console.log(o)
+		}
+	});
+		// 工具栏参数与lay-filter匹配
+		this.treeGrid.on('tool('+obj.config.id+')', (obj) => {
+				if(obj.event === 'del'){//删除行
+						this.del(obj);
+				}else if(obj.event==="add"){//添加行
+						this.add(obj.data);
+				}
+		});
+		});
+	},
+	del: function (obj) {
+		layer.confirm("你确定删除数据吗？", {icon: 3, title:'提示'},
+				(index) => {//确定回调
+						tool.Ajax(`${this.par.apiUrl}/ics/sysorg/delete/${obj.data.id}`, {}, "delete")
+						.then((data) => {
+							obj.del();
+							console.log(data);
+						}, (re) => {
+							console.log(re);
+						})
+						layer.close(index);
+				},function (index) {//取消回调
+					 layer.close(index);
+				}
+		);
+  },
+	add: function (obj) {
+		this.par.addParams = {
+			email: "123@qq.com",
+			orgName:"人事部",
+			orgCode: "rsb",
+			fullName: "英卡人事部"
+		}
+		// 组装必须的parentCode，parentCodes
+		let code = {parentCode: obj.orgCode, parentCodes: `${obj.parentCodes}${obj.orgCode},`}
+		let params = Object.assign(code, this.par.addParams);
+		tool.Ajax(`${this.par.apiUrl}/ics/sysorg/save`, params, "post")
+		.then((data) =>{
+			if(data.code === "200") {
+				this.query(this.par.config.id, obj);
+			}
+		})
+  },
+	// 重载
+	reload: function () {
+		this.treeGrid.reload(tableId,{
+				page:{
+						curr:1
+				}
+		});
+	},
+	// 刷新数据
+	query: function (id, obj) {
+		tool.Ajax(`${this.par.apiUrl}/ics/sysorg/list`, {}, "get")
+		.then(() => {
+			this.treeGrid.query(id, {
+				where:{
+						name:obj.orgName
+				}
+		  });
+		})
+ },
+	recuNodes: function(child) {
+		child.map(item => {
+				item.name = item.orgName;
+				item.spread = false; // 默认不展开
+				if(item.children.length) this.recuNodes(item.children)
+		})
 	},
 	on: function(type, handler){
 		// type: detail, del, edit
