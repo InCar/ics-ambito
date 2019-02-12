b763b26f7f1f0632692e1b4f0e5a7594a4bc40f5
wellingtonjone <509750512@qq.com>
2019-02-12 10:08:57 +0800

    aop

--- src/main/java/com/incarcloud/ics/ambito/security/config/AopEnableSecurityConfiguration.java
+++ src/main/java/com/incarcloud/ics/ambito/security/config/AopEnableSecurityConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 
 /**
  * @description
- *  该类继承自 {@link DefaultSecurityConfiguration}，用于开启注解方式的权限验证，直接通过注解如@RequiresRoles对方法进行拦截并验证权限，
+ *  该类继承�? {@link DefaultSecurityConfiguration}，用于开启注解方式的权限验证，直接�?�过注解如@RequiresRoles对方法进行拦截并验证权限�?
  *  拦截机制使用了spring的aop，需要开启注解权限验证，继承该配置类即可，配置方式和{@link DefaultSecurityConfiguration}相同
  * @version 1.0
  * @author ThomasChan
